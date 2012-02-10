b5aee4fb2533d42d66c5c91a6a0b0d9f017d84a4
Rob Beane <robbeane@gmail.com>
2012-02-10 15:42:38 -0500

    remove prebuilt Camera.apk

--- google-vendor.mk
+++ google-vendor.mk
@@ -16,7 +16,6 @@
 PRODUCT_COPY_FILES += \
     vendor/google/prebuilt/app/CalendarGoogle.apk:system/app/CalendarGoogle.apk \
     vendor/google/prebuilt/app/CalendarProvider.apk:system/app/CalendarProvider.apk \
-    vendor/google/prebuilt/app/Camera.apk:system/app/Camera.apk \
     vendor/google/prebuilt/app/CarHomeGoogle.apk:system/app/CarHomeGoogle.apk \
     vendor/google/prebuilt/app/ChromeBookmarksSyncAdapter.apk:system/app/ChromeBookmarksSyncAdapter.apk \
     vendor/google/prebuilt/app/EmailGoogle.apk:system/app/EmailGoogle.apk \
