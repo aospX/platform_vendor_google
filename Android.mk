7ae95e5be675b2ddddb628ce05db0c8e8574a3fa
Rob Beane <robbeane@gmail.com>
2012-07-23 01:16:42 -0400

    Remove building QuickSearchBox from source
    Remove building of QuickSearchBox since we use an updated
    prebuilt apk.
    
    Change-Id: I0578d37783eb223adb8687f0ff833c5ad04f29fe

--- Android.mk
+++ Android.mk
@@ -1,2 +1,2 @@
 # We need a way to prevent the stuff Google Apps replaces from being included in the build. This is a hacky way to do that.
-PACKAGES.Phone.OVERRIDES := Email Exchange Gallery2 PicoTts
+PACKAGES.Phone.OVERRIDES := Email Exchange Gallery2 PicoTts QuickSearchBox
